f5ea6117db756134b2ce828c25ed6b98c2726f8f
Joshua Cutting <cuttingj@ainfosec.com>
2024-04-23 18:18:23 -0400

    windowed

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ REM Ensuring python dependencies are met
 python3 -m pip install -r requirements.txt
 
 REM Creating the executable
-python3 -m PyInstaller --onefile .\src\main.py 
+python3 -m PyInstaller --windowed --onefile .\src\main.py 
 
 REM Creating the installation file
 ISCC.exe installer.iss
